5884c166363bdb8de69d9b26a390553b395ae36b
Maxim Casiana <casiana2075@gmail.com>
2025-11-07 16:30:20 +0200

    JVNGRS-10 Added gateway maven module, class GatewayApplication and a README

---
+++ back-end/services/gateway/src/main/java/com/project/mentorship/service/gateway/GatewayApplication.java
@@ -0,0 +1,7 @@
+package com.project.mentorship.service.gateway;
+
+public class GatewayApplication {
+    public static void main(String[] args) {
+        System.out.printf("Hello from service gateway module!");
+    }
+}
