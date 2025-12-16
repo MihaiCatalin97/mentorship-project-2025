391063ef636ce21f3e99a1a12bdacc779eaebe3f
Maxim Casiana <casiana2075@gmail.com>
2025-12-16 17:27:11 +0200

    JVNGRS-41 Create contracts for users/{id}

---
+++ back-end/services/auth-service/src/main/java/com/project/mentorship/service/auth/config/AuthSpringConfiguration.java
@@ -0,0 +1,9 @@
+package com.project.mentorship.service.auth.config;
+
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
+
+@ComponentScan(basePackages = "com.project.mentorship.service.auth.**")
+@Configuration
+public class AuthSpringConfiguration {
+}
