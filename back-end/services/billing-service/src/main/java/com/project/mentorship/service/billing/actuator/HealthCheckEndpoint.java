75b36cdeb2f7f7bb0c11d837b7c37df1f65b703b
Emanuel.Luculescu <emyluculescu@gmail.com>
2025-12-18 14:11:21 +0200

    JVNGRS-71 added spring boot actuator + HealthCheckEndpoint

---
+++ back-end/services/billing-service/src/main/java/com/project/mentorship/service/billing/actuator/HealthCheckEndpoint.java
@@ -0,0 +1,17 @@
+package com.project.mentorship.service.billing.actuator;
+
+import org.springframework.boot.actuate.endpoint.annotation.Endpoint;
+import org.springframework.boot.actuate.endpoint.annotation.ReadOperation;
+import org.springframework.stereotype.Component;
+
+import java.util.Map;
+
+@Component
+@Endpoint(id = "healthcheckendpoint")
+public class HealthCheckEndpoint {
+
+    @ReadOperation
+    public Map<String, String> health() {
+        return Map.of("status", "UP");
+    }
+}
